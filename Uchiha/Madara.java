c1e9f27ccbad9be02d0cc71dd8ba8be65703fbbc
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-03-03 21:09:32 +0530

    [ADDED]:Dipak - Create Package Uchiha In Java

---
+++ Uchiha/Madara.java
@@ -0,0 +1,9 @@
+package Uchiha;
+public class Madara{
+	public void Obito(){
+		System.out.println("Everybody No Loves Mi");
+	}
+	public void Itachi(){
+		System.out.println("Every Sacrifice");
+	}
+}
